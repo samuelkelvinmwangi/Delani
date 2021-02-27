d85efabb0b6821f702cbeb15ff9164275f8d8749
Samuel Mwangi <samuelkelvinmwangi@gmail.com>
2021-02-27 16:31:37 +0300

    adding javascript

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
-// var a;
-// function show_hide(){
+//Portfolio settings
+const lightbox=document.querySelector(".lightbox"),
+lightboxImg = lightbox.querySelector(".lightbox-img"),
+lightboxClose = lightbox.querySelector(".lightbox-close"),
+lightboxText = lightbox.querySelector(".caption-text"),
+lightboxCounter = lightbox.querySelector(".caption-counter");
+let itemIndex=0;
 
-//     if(a==1){
-//         document.getElementById("1").style.display="inline";
-//         return a=0;
-//     }
-//     else{
-//         document.getElementById("1").style.display="none";
-//         return a=1;
-//     }
-// }
+for(let i=0; i<totalPortfolioItem; i++){
+   portfolioItems[i].addEventListener("click",function(){
+       itemIndex=i;
+       changeItem();
+       toggleLightbox();
+   }) 
+}
+function nextItem(){
+    if (itemIndex==totalPortfolioItem-1){
+        itemIndex=0;
+    }
+    else{
+        itemIndex++
+    }
+    changeItem();
+}
