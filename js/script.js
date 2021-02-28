8b1643468a3adf1233ac58977612ac1b086abde7
Samuel Mwangi <samuelkelvinmwangi@gmail.com>
2021-02-28 23:00:11 +0300

    adding java

--- js/script.js
+++ js/script.js
@@ -24,3 +24,55 @@ $(document).ready(function(){
               $('#pm-image').slideDown('1500');
             })
     })
+
+
+
+    $(document).ready(function(){
+        $("#i1").mouseover(function(){
+         $("#w1").show();
+        }).mouseout(function(){
+          $("#w1").hide();
+        });
+
+        $("#i2").mouseover(function(){
+            $("#w2").show();
+          }).mouseout(function(){
+            $("#w2").hide();
+          });
+
+          $("#i3").mouseover(function(){
+            $("#w3").show();
+          }).mouseout(function(){
+            $("#w3").hide();
+          });
+
+          $("#i4").mouseover(function(){
+            $("#w4").show();
+          }).mouseout(function(){
+            $("#w4").hide();
+          });
+
+          $("#i5").mouseover(function(){
+            $("#w5").show();
+          }).mouseout(function(){
+            $("#w5").hide();
+          });
+
+          $("#i6").mouseover(function(){
+            $("#w6").show();
+          }).mouseout(function(){
+            $("#w6").hide();
+          });
+
+          $("#i7").mouseover(function(){
+            $("#w7").show();
+          }).mouseout(function(){
+            $("#w7").hide();
+          });
+
+          $("#i8").mouseover(function(){
+            $("#w8").show();
+          }).mouseout(function(){
+            $("#w8").hide();
+          });
+    })
